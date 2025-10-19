57fe9f9d85a139001fad371d73ac3f4c0702a18d
cassauni <vasaby2004@gmail.com>
2025-10-19 14:32:55 +0300

    Done

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu   sync.RWMutex
+	data map[int]int
+}
+
+func NewCache(capacity int) *Cache {
+	return &Cache{data: make(map[int]int, capacity)}
+}
+
+func (c *Cache) Get(key int) int {
+	c.mu.RLock()         // Блокируем на чтение
+	defer c.mu.RUnlock() // Освобождаем после чтения
+	value, ok := c.data[key]
+	if !ok {
+		fmt.Println("Cache miss") // Ключ не найден
+		return 0
+	}
+	return value
+}
+
+func (c *Cache) Set(key int, value int) {
+	c.mu.Lock()
+	c.data[key] = value
+	c.mu.Unlock()
+}
+
+func main() {
+	cache := NewCache(30)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 30; i++ {
+			cache.Set(i, i)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 30; i++ {
+			time.Sleep(100 * time.Millisecond)
+			value := cache.Get(i)
+			fmt.Printf("value of map[%d] is %d\n", i, value)
+		}
+	}()
+
+	wg.Wait()
+}
